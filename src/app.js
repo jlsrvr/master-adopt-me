9908d45a07bc1ed83abd3a9f176e21d255934ed8
Jules Rivoire <julesriv@gmail.com>
2020-03-27 17:02:12 +0100

    set up tooling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
-const Pet = ({ name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-    ]);
+    React.createElement("h2", {}, breed),
+  ]);
 };
-
 const App = () => {
-  return React.createElement("div", {id: 'something-important'}, [
+  return React.createElement("div", { id: "something-important" }, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Polette", animal: "Tondeuse", breed: "verte"}),
-    React.createElement(Pet, { name: "Toy", animal: "Chien", breed: "lab"}),
-    React.createElement(Pet, { name: "mistache", animal: "chat", breed: "petit"}),
-    ]);
+    React.createElement(Pet, {
+      name: "Polette",
+      animal: "Tondeuse",
+      breed: "verte",
+    }),
+    React.createElement(Pet, {
+      name: "Toy",
+      animal: "Chien",
+      breed: "lab",
+    }),
+    React.createElement(Pet, {
+      name: "mistache",
+      animal: "chat",
+      breed: "petit",
+    }),
+  ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
